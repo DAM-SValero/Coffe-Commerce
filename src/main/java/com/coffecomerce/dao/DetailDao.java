d8690118030a671e26396cfdf72f891b4564a127
Juan Senen <juansenen@hotmail.com>
2022-06-14 18:37:24 +0200

    DAO inicial CATEGORYDAO - DETAILDAO - ORDERDAO

--- src/main/java/com/coffecomerce/dao/DetailDao.java
+++ src/main/java/com/coffecomerce/dao/DetailDao.java
@@ -1,4 +1,12 @@
 package com.coffecomerce.dao;
 
+import java.sql.Connection;
+
 public class DetailDao {
+
+    private Connection connection;
+
+    public DetailDao (Connection connection){
+        this.connection = connection;
+    }
 }
