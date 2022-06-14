1bb0bd04823288f4c0c34f5c6fe930a9f4261ad8
A8-PC00\A8-PC00 <ss>
2022-06-14 14:00:41 +0200

    Subida Estructura Inicial

---
+++ src/main/java/com/coffecomerce/util/Constants.java
@@ -0,0 +1,14 @@
+package com.coffecomerce.util;
+
+import java.security.SecureRandom;
+
+public class Constants {
+
+    // Cadenas constantes para uso a la conexión JDBC con la base de datos
+    public static String ORCDRV="oracle.jdbc.driver.OracleDriver";
+    public static String ORCURL="jdbc:oracle:thin:@//localhost:1521/xe";
+    public static String ORCUSER="coffee";
+    public static String ORCPASS="1234";
+
+
+}
