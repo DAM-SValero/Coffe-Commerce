380173ebce9dcdf462a7b0cdf4c3039a72a3360a
A8-PC00\A8-PC00 <ss>
2022-06-14 18:37:56 +0200

    Domain USERDAO-PRODUCTDAO-PROVIDERDAO

--- src/main/java/com/coffecomerce/dao/ProviderDao.java
+++ src/main/java/com/coffecomerce/dao/ProviderDao.java
@@ -1,4 +1,16 @@
 package com.coffecomerce.dao;
 
+import java.sql.Connection;
+
 public class ProviderDao {
+
+    private Connection connection;
+
+    /**
+     * CONSTRUCTOR PARA CONECTAR CON LA BBDD
+     */
+    public ProviderDao(Connection connection) {
+        this.connection = connection;
+    }
+
 }
--- src/main/java/com/coffecomerce/dao/UserDao.java
+++ src/main/java/com/coffecomerce/dao/UserDao.java
@@ -1,4 +1,16 @@
 package com.coffecomerce.dao;
 
+import java.sql.Connection;
+
 public class UserDao {
+
+    private Connection connection;
+
+    /**
+     * CONSTRUCTOR PARA CONECTAR CON LA BBDD
+     */
+    public UserDao(Connection connection) {
+        this.connection = connection;
+    }
+
 }
